402e662875060b361f1c5005024cefc5dde0276c
Maxim <48493427+MaximYarmoliuk@users.noreply.github.com>
2019-08-21 19:56:08 +0300

    Update task-3.js

--- goit-js-hw-5/js/task-3.js
+++ goit-js-hw-5/js/task-3.js
@@ -1,21 +1,26 @@
 "use strict";
 
-const Storage = function(items) {
-  this.items = items;
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
 
-  this.getItems = () => this.items;
+  getItems() {
+    return this.items;
+  }
 
-  this.addItem = item => this.items.push(item);
+  addItem(item) {
+    this.items.push(item);
+  }
 
-  this.removeItem = function(item) {
+  removeItem(item) {
     let idx = this.items.indexOf(item);
     if (idx !== -1) {
       return this.items.splice(idx, 1);
     }
     return this.items;
-  };
-};
-
+  }
+}
 const storage = new Storage([
   "Нанитоиды",
   "Пролонгер",
@@ -31,3 +36,4 @@ console.log(storage.items); // [ "Нанитоиды", "Пролонгер", "Ж
 
 storage.removeItem("Пролонгер");
 console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
